430a55bef317d86ddea207c314cfa3a15046244c
SuzuKanae <melesgabriele@gmail.com>
2023-10-05 17:44:18 +0200

    optimasetions

--- EntropyCalculator.py
+++ EntropyCalculator.py
@@ -2,37 +2,27 @@
 import sys
 
 #open file and read text(text)
-with open("texts/"+sys.argv[1], "r") as file:
-    text = file.read()
+f = open("texts/"+sys.argv[1], "rb")
+text = f.read()
+f.close()
     
 #calculation of letters
 letter_count = {}
-character_count = {}
 text_length = len(text)
+print(text_length)
 
 #counting cycle
-for letter in text:
-    if letter.isalpha():
-        if letter in letter_count:
-            letter_count[letter]['count'] += 1
-        else:
-            letter_count[letter] = {'count':1, 'percentage':0}
-    else:
-        if letter in letter_count:
-            character_count[letter]['count'] += 1
-        else:
-            character_count[letter] = {'count': 1, 'percentage': 0}
-            
-#I combine the two dictionaries
-count_complete = {**letter_count, **character_count}
+for letter in set(text):
+    letter_count[chr(letter)] = text.count(letter)
 
 #calculation of the percentage
-for lettera, info in count_complete.items():
-    info['percentage'] = (info['count'] / text_length) * 100
+for letter, count in letter_count.items():
+    letter_count[letter] = {'count': count, 'percentage': count / text_length * 100}
 
 #sort the dictionary
-count_complete_sorted = dict(sorted(count_complete.items(), key=lambda x: x[1]['percentage'], reverse=True))
+letter_count_sorted = dict(sorted(letter_count.items(), key=lambda x: x[1]['percentage'], reverse=True))
 
 #print the result as output
-for letter, info in count_complete_sorted.items():
+for letter, info in letter_count_sorted.items():
     print(f"'{letter}': count: {info['count']}, percentage: {info['percentage']:.2f}%")
+    
